cc25762e92e0542513358f1b8313a83519e4508f
Ihor Chovpan <ihor@localhost.localdomain>
2023-11-28 11:17:59 -0500

    Sorted up the repository structure.

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # game-of-life
 My implementation of [Conway's Game of life](https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life).
-
-
--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,29 @@
-use wasm_bindgen::prelude::*;
-use web_sys::console;
-use web_sys::Node;
-use web_sys::Document;
+use cfg_if::cfg_if;
 
-// When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
-// allocator.
-//
-// If you don't want to use `wee_alloc`, you can safely delete this.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-
-// This is like the `main` function, except for JavaScript.
-#[wasm_bindgen(start)]
-pub fn main_js() -> Result<(), JsValue> {
-    // This provides better error messages in debug mode.
-    // It's disabled in release mode so it doesn't bloat up the file size.
-    #[cfg(debug_assertions)]
-    console_error_panic_hook::set_once();
-    
-    use wasm_bindgen::JsCast;
-
-    let window = web_sys::window().unwrap();
-    let document = window.document().unwrap();
-    let html_document = document.dyn_into::<web_sys::HtmlDocument>().unwrap();
-    let cookie = html_document.cookie().unwrap();
-    let p: web_sys::Node = document.create_element("p")?.into();
-
-    Ok(())
+cfg_if! {
+    if #[cfg(feature = "wee_alloc")] {
+        #[global_allocator]
+        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+    }
 }
+
+#[allow(unused_variables)]
+fn main() {
+    extern crate wasm_bindgen;
+
+    use wasm_bindgen::prelude::*;
+
+    #[wasm_bindgen]
+    extern {
+        fn alert(s: &str);
+    }
+    
+    #[wasm_bindgen]
+    pub fn greet() {
+        utils::set_panic_hook();
+        alert("Hello from Ihor");
+        panic!("wtf");
+    }
+}
+
+mod utils;
