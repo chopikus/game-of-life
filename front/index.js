872886e9e4ed16d3ec228a09e2409f08be92144f
Ihor Chovpan <ihor@localhost.localdomain>
2023-12-07 18:10:02 -0500

    Implement pan and zoom on an infinite grid

--- front/index.js
+++ front/index.js
@@ -1,3 +1,9 @@
+let viewX = 0;
+let viewY = 0;
+let scale = 20;
+let w = 0;
+let h = 0;
+
 function main() {
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
@@ -7,51 +13,79 @@ function main() {
     function resizeCanvas() {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
-        drawGrid(ctx);
-        fillCells(ctx);
+        addListeners(ctx);
+        draw(ctx);
     }
 
-    resizeCanvas(); 
+    resizeCanvas();
 }
 
 
-const cell_size = 50;
-const offset_x = 20;
-const offset_y = 20;
-const line_thickness = 1;
+let mouseStart = null;
 
-function drawGrid(ctx) {
-    const w = ctx.canvas.width;
-    const h = ctx.canvas.height;
+function addListeners(ctx) {
+    function reset() {
+        mouseStart = null;
+    }
+    ctx.canvas.addEventListener("mousedown", e => {
+        mouseStart = {x: e.clientX, y: e.clientY};
+    });
+    ctx.canvas.addEventListener("mouseup", e => {
+        reset();
+    });
+    ctx.canvas.addEventListener("mouseleave", e => {
+        reset();
+    });
+    ctx.canvas.addEventListener("wheel", e => {
+        scale *= Math.pow(2, e.deltaY / 1000);
+        draw(ctx);
+    });
+    ctx.canvas.addEventListener("mousemove", e => {
+        if (!mouseStart)
+            return;
+        dx = e.clientX - mouseStart.x;
+        dy = e.clientY - mouseStart.y;
+        viewX -= dx / scale;
+        viewY -= dy / scale;
+        draw(ctx);
+        mouseStart = {x: e.clientX, y: e.clientY};
+    });
+}
+
+function cellCoordsToScreen(x, y) {
+    return {x: (x - viewX) * scale, 
+            y: (y - viewY) * scale};
+}
+
+function clear(ctx) {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
+
+function draw(ctx) {
+    clear(ctx);
+    w = ctx.canvas.width / scale;
+    h = ctx.canvas.height / scale;
     ctx.strokeStyle = "#000000";
-    ctx.lineWidth = line_thickness;
-    const cellX = Math.ceil(w / cell_size);
-    const cellY = Math.ceil(h / cell_size);
+    ctx.lineWidth = 1 * scale/50;
     ctx.beginPath();
-    for (let x = 0; x < cellX; x += 1) {
-        ctx.moveTo(grid_start + x * cell_size, 0);
-        ctx.lineTo(grid_start + x * cell_size, h);
+    for (let x = Math.floor(viewX); x <= Math.ceil(viewX + w); x += 1) {
+        let start = cellCoordsToScreen(x, viewY);
+        let end = cellCoordsToScreen(x, viewY + h);
+        ctx.moveTo(start.x + 0.5, start.y + 0.5);
+        ctx.lineTo(end.x + 0.5, end.y + 0.5);
         ctx.stroke();
     }
-    for (let y = 0; y < cellY; y += 1) {
-        ctx.moveTo(0, grid_start + y * cell_size);
-        ctx.lineTo(w, grid_start + y * cell_size);
+    for (let y = Math.floor(viewY); y <= Math.ceil(viewY + h); y += 1) {
+        let start = cellCoordsToScreen(viewX, y);
+        let end = cellCoordsToScreen(viewX + w, y);
+        ctx.moveTo(start.x + 0.5, start.y + 0.5);
+        ctx.lineTo(end.x + 0.5, end.y + 0.5);
         ctx.stroke();
     }
+    let {x, y} = cellCoordsToScreen(0, 0);
+    ctx.fillRect(x, y, scale, scale);
     ctx.closePath();
-}
-
-function fillCells(ctx) {
-    ctx.fillStyle = "yellow";
-    cells_to_fill = [[0, 0], [5, 5], [10, 10]];
-    for (const cell of cells_to_fill) {
-        let x = cell[0];
-        let y = cell[1];
-        ctx.fillRect(grid_start + (x-1) * cell_size + line_thickness / 2,
-                     grid_start + (y-1) * cell_size + line_thickness / 2,
-                     cell_size - line_thickness, 
-                     cell_size - line_thickness);
-    }
+    console.log(scale);
 }
 
 main();
