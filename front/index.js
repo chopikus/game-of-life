bf9d653ab585d8b512dcc793139d86e3d9822312
Ihor Chovpan <ihor@localhost.localdomain>
2023-12-17 19:06:19 -0500

    Minor cleanup.

--- front/index.js
+++ front/index.js
@@ -1,6 +1,6 @@
 let viewX = 0;
 let viewY = 0;
-let scale = 20;
+let scale = 50;
 let w = 0;
 let h = 0;
 
@@ -23,23 +23,20 @@ function main() {
 
 let mouseStart = null;
 
-function addListeners(ctx) {
-    function reset() {
-        mouseStart = null;
-    }
+function addListeners(ctx) { 
     ctx.canvas.addEventListener("mousedown", e => {
         mouseStart = {x: e.clientX, y: e.clientY};
     });
-    ctx.canvas.addEventListener("mouseup", e => {
-        reset();
-    });
-    ctx.canvas.addEventListener("mouseleave", e => {
-        reset();
-    });
+
+    ["mouseup", "mouseleave"].forEach(name => ctx.canvas.addEventListener(name, _ => {
+        mouseStart = null;
+    }));
+
     ctx.canvas.addEventListener("wheel", e => {
         scale *= Math.pow(2, e.deltaY / 1000);
         draw(ctx);
     });
+
     ctx.canvas.addEventListener("mousemove", e => {
         if (!mouseStart)
             return;
@@ -57,35 +54,41 @@ function cellCoordsToScreen(x, y) {
             y: (y - viewY) * scale};
 }
 
-function clear(ctx) {
+function draw(ctx) {
+    // clearing then drawing grid
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    drawGrid(ctx); 
 }
 
-function draw(ctx) {
-    clear(ctx);
+function drawGrid(ctx) {
     w = ctx.canvas.width / scale;
     h = ctx.canvas.height / scale;
     ctx.strokeStyle = "#000000";
-    ctx.lineWidth = 1 * scale/50;
-    ctx.beginPath();
-    for (let x = Math.floor(viewX); x <= Math.ceil(viewX + w); x += 1) {
-        let start = cellCoordsToScreen(x, viewY);
-        let end = cellCoordsToScreen(x, viewY + h);
-        ctx.moveTo(start.x + 0.5, start.y + 0.5);
-        ctx.lineTo(end.x + 0.5, end.y + 0.5);
-        ctx.stroke();
-    }
-    for (let y = Math.floor(viewY); y <= Math.ceil(viewY + h); y += 1) {
-        let start = cellCoordsToScreen(viewX, y);
-        let end = cellCoordsToScreen(viewX + w, y);
-        ctx.moveTo(start.x + 0.5, start.y + 0.5);
-        ctx.lineTo(end.x + 0.5, end.y + 0.5);
-        ctx.stroke();
+    drawing = true;
+    if (scale >= 40)
+        ctx.lineWidth = Math.ceil(scale / 50);
+    else
+        drawing = false;
+    if (drawing) {
+        ctx.beginPath();
+        for (let x = Math.floor(viewX); x <= Math.ceil(viewX + w); x += 1) {
+            let start = cellCoordsToScreen(x, viewY);
+            let end = cellCoordsToScreen(x, viewY + h);
+            ctx.moveTo(start.x + 0.5, start.y + 0.5);
+            ctx.lineTo(end.x + 0.5, end.y + 0.5);
+            ctx.stroke();
+        }
+        for (let y = Math.floor(viewY); y <= Math.ceil(viewY + h); y += 1) {
+            let start = cellCoordsToScreen(viewX, y);
+            let end = cellCoordsToScreen(viewX + w, y);
+            ctx.moveTo(start.x + 0.5, start.y + 0.5);
+            ctx.lineTo(end.x + 0.5, end.y + 0.5);
+            ctx.stroke();
+        }
     }
     let {x, y} = cellCoordsToScreen(0, 0);
     ctx.fillRect(x, y, scale, scale);
     ctx.closePath();
-    console.log(scale);
 }
 
 main();
