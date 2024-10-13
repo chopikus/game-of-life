2363eb0f7bf2146d42e8afcec831d794858d0fb1
Ihor Chovpan <i.chovpan@gmail.com>
2024-10-13 12:28:31 -0400

    Benchmark transferring data between wasm on a separate thread and main thread

--- front/index.js
+++ front/index.js
@@ -3,7 +3,13 @@ const myWorker = new Worker('worker.js', {type: 'module'});
 console.log('passing message to the worker..');
 myWorker.postMessage('hello to the worker');
 
+let i = 0;
+console.time("cells2");
 myWorker.onmessage = function (event) {
   const bar = new Uint8Array(event.data);
   console.log(bar.length);
+  i++;
+  if (i==1000) {
+    console.timeEnd("cells2");
+  }
 };
--- src/lib.rs
+++ src/lib.rs
@@ -9,8 +9,30 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn read(x: String) {
-    let y = SharedArrayBuffer::new(1024);
+struct Universe {
+    output: Vec<u8>,  
+}
 
-    log(&x);
+#[wasm_bindgen]
+impl Universe {
+    pub fn new() -> Universe {
+        Universe {
+            output: vec![]
+        }
+    }
+
+    pub fn read(&mut self, x: String) -> *const u8 { 
+        //log(&x);
+        
+        let mut my_vec= vec![];
+        
+        for i in 0..1048576 {
+            let val = (i % 255) as u8;
+            my_vec.push(val);
+        }
+
+        self.output = my_vec;
+
+        return self.output.as_ptr();
+    }   
 }
