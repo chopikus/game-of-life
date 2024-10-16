8d937aa6a4deb884b98ad1e68273ca52e352196c
Ihor Chovpan <i.chovpan@gmail.com>
2024-10-16 14:41:37 -0400

    Send messages between worker and main thread.

--- front/worker.js
+++ front/worker.js
@@ -1,25 +1,47 @@
 import wasmInit, { Universe } from './pkg/game_of_life_rust.js';
 
-let onMessageResponse = async () => {
-    const rustWasm = await wasmInit("./pkg/game_of_life_rust_bg.wasm");
-    let memory = rustWasm.memory;
-    
-    // sending response to main thread
-    //const u = new Uint8Array(1024 * 1024 * 8);//.map((v, i) => 0);
-    //console.log("created u");
-    const universe = Universe.new();
-    
-    console.time("cells");
-    for (let i = 0; i<1000; i++) {
-        const cellsPtr = universe.read("hello world 123123123");
-        const cells = new Uint8Array(memory.buffer, cellsPtr, 1048576);
+let rustWasm, universe;
+
+let onMessageResponse = async (data) => {
+    switch (data.req) {
+        case "init": {
+            rustWasm = await wasmInit("./pkg/game_of_life_rust_bg.wasm");
+            postMessage({res: "initOk"});
+            break;
+        }
         
-        const cellsToPass = new Uint8Array(cells);
-        postMessage(cellsToPass.buffer, [cellsToPass.buffer]);
+        case "file": {
+            let s = data.s;
+            if (universe)
+                universe.free();
+            universe = Universe.new(s);
+            postMessage({res: "fileOk"});
+            break;
+        }
+
+        case "tick": {
+            universe.tick();
+            postMessage({res: "tickOk"});
+            break;
+        }
+
+        case "cell": {
+            universe.req_output();
+            const outputPtr = universe.output();
+            const outputLen = universe.output_len();
+            const cells0 = new BigInt64Array(rustWasm.memory.buffer, outputPtr, outputLen);
+            const cells = new BigInt64Array(cells0);
+
+            postMessage({res: "cellOk", buf: cells.buffer}, [cells.buffer]);
+            break;
+        }
+
+        default: {
+            break;
+        }
     }
-    console.timeEnd("cells");
 };
 
 onmessage = (event) => {
-    onMessageResponse();
+    onMessageResponse(event.data);
 };
